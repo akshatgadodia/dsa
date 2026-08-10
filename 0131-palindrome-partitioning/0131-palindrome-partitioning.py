76dc3011abb727489f6d06fb8f67c7af9cf42947
AKSHAT GADODIA <59470728+akshatgadodia@users.noreply.github.com>
2026-08-10 20:38:09 +0530

    Time: 43 ms (61.39%), Space: 34.2 MB (31.78%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,28 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+        current = []
+
+        def is_palindrome(word):
+            return word == word[::-1]
+
+        def backtrack(start):
+            # We have partitioned the entire string
+            if start == len(s):
+                result.append(current[:])
+                return
+
+            # Try every possible substring starting at `start`
+            for end in range(start + 1, len(s) + 1):
+                substring = s[start:end]
+
+                if is_palindrome(substring):
+                    current.append(substring)
+
+                    backtrack(end)
+
+                    current.pop()
+
+        backtrack(0)
+
+        return result
