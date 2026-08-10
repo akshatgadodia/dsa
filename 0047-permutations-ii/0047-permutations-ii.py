7770fce2dd9ff4572a66994c4051958dd7fcea8e
AKSHAT GADODIA <59470728+akshatgadodia@users.noreply.github.com>
2026-08-10 21:02:55 +0530

    Time: 3 ms (90.49%), Space: 20 MB (24.33%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -2,16 +2,22 @@ class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         result, current = [], []
         used = [False] * len(nums)
-
+        
         def backtrack():
             if len(current) == len(nums):
-                if current[:] not in result:
-                    result.append(current[:])
+                result.append(current[:])
                 return 
             
+            used_at_level = set()
+
             for index in range(len(nums)):
                 if used[index]:
                     continue
+
+                if nums[index] in used_at_level:
+                    continue
+                used_at_level.add(nums[index])
+                
 
                 current.append(nums[index])
                 used[index] = True
