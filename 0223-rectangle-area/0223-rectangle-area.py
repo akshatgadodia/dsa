ad0e0ae53c851e236ec349ea855655952833aa4c
AKSHAT GADODIA <59470728+akshatgadodia@users.noreply.github.com>
2026-08-16 13:21:59 +0530

    Time: 0 ms (100%), Space: 19.2 MB (98.11%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.py
+++ 0223-rectangle-area/0223-rectangle-area.py
@@ -10,15 +10,11 @@ class Solution:
         area_r2 = compute_area_of_rectange(bx1, bx2, by1, by2)
 
         common_area = 0
-        if (
-            (ax1 <= bx1 < ax2) or (ax1 < bx2 <= ax2) or (bx1 <= ax1 < bx2) or (bx1 < ax2 <= bx2)
-        ) and (
-            (ay1 <= by1 < ay2) or (ay1 < by2 <= ay2) or (by1 <= ay1 < by2) or (by1 < ay2 <= by2)
-        ):
-            cx1 = max(ax1, bx1)
-            cx2 = min(ax2, bx2)
-            cy1 = max(ay1, by1)
-            cy2 = min(ay2, by2)
+        cx1 = max(ax1, bx1)
+        cx2 = min(ax2, bx2)
+        cy1 = max(ay1, by1)
+        cy2 = min(ay2, by2)
+        if cx1 < cx2 and cy1 < cy2:
             common_area = compute_area_of_rectange(cx1, cx2, cy1, cy2)
 
         return area_r1 + area_r2 - common_area
