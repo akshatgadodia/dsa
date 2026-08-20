b8ad4d77a0c3c3952f9e09c58890af98376d29ce
AKSHAT GADODIA <59470728+akshatgadodia@users.noreply.github.com>
2026-08-20 13:19:25 +0530

    Time: 6 ms (42.33%), Space: 19.2 MB (64.24%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,52 +1,26 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        num = str(num)
-        n = len(num)
+        values = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I")
+        ]
 
-        def calculate_result(digit, digit_values):
-            if digit <= 3:
-                return digit_values[1] * digit
-            elif digit == 4:
-                return digit_values[1] + digit_values[5]
-            elif 5 < digit <= 8:
-                return digit_values[5] + (digit_values[1] * (digit - 5))
-            elif digit == 9:
-                return digit_values[1] + digit_values[10]
-            
-            return digit_values[digit]
+        result = ""
 
-        result = ""            
-
-        values = {
-            1: {
-                1: 'I',
-                5: 'V',
-                10: 'X'
-            },
-            2: {
-                1: 'X',
-                5: 'L',
-                10: 'C'
-            },
-            3: {
-                1: 'C',
-                5: 'D',
-                10: 'M'
-            },
-            4: {
-                1: 'M',
-            }
-        }
-
-        for i in range(n - 1, -1, -1):
-            digit = int(num[i])
-
-            digit_values = values[n - i]
-
-            digit_result = calculate_result(digit, digit_values)
-            # print(digit, digit_values, digit_result)
-
-            result = digit_result + result
+        for value, symbol in values:
+            while num >= value:
+                result += symbol
+                num -= value
 
         return result
-
