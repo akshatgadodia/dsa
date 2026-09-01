3f462e4197c69e73600840e33e15a540c12d0468
AKSHAT GADODIA <59470728+akshatgadodia@users.noreply.github.com>
2026-09-01 10:51:37 +0530

    Time: 20 ms (55.57%), Space: 34.2 MB (6.32%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -1,22 +1,15 @@
 class Solution:
     def minimumDeletions(self, nums: List[int]) -> int:
         n = len(nums)
-        min_ele_idx, max_ele_index = 0, 0
 
-        for idx in range(1, n):
-            value = nums[idx]
+        min_idx = nums.index(min(nums))
+        max_idx = nums.index(max(nums))
 
-            if value < nums[min_ele_idx]:
-                min_ele_idx = idx
-            if value > nums[max_ele_index]:
-                max_ele_index = idx
-            
-        remove_from_left = max(min_ele_idx, max_ele_index) + 1
-        remove_from_right = n - min(min_ele_idx, max_ele_index)
+        left = min(min_idx, max_idx)
+        right = max(min_idx, max_idx)
 
-        remmove_first_from_left = min(min_ele_idx, max_ele_index) + 1
-        remove_last_from_right = n - max(min_ele_idx, max_ele_index)
-        remove_from_both_ends = remmove_first_from_left + remove_last_from_right
+        remove_left = right + 1
+        remove_right = n - left
+        remove_both = (left + 1) + (n - right)
 
-        return min(remove_from_left, remove_from_right, remove_from_both_ends)
-        
+        return min(remove_left, remove_right, remove_both)
